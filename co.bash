79673ae2afa1244a733057c327006ff38ba84cc5
nobody1331 <130770326+nobody1331@users.noreply.github.com>
2023-07-13 18:55:26 +0700

    Add files via upload

---
+++ co.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download file dari URL
+wget https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-full-2023.04.01-x86_64.iso
+
+# Hapus file setelah selesai didownload
+rm blackarch-linux-full-2023.04.01-x86_64.iso
